3e57c619a1436c16349cdbc537af2f642983ee6e
namjincho <skawls5028@gmail.com>
2019-08-14 19:19:18 +0900

    Google maps

--- app/src/main/java/com/dev/jungmo/jungmo_diary/MainActivity.kt
+++ app/src/main/java/com/dev/jungmo/jungmo_diary/MainActivity.kt
@@ -1,17 +1,30 @@
 package com.dev.jungmo.jungmo_diary
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.fragment.app.FragmentActivity
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : FragmentActivity(), OnMapReadyCallback {
+    private lateinit var googleMaps: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var fragment = getSupportFragmentManager().findFragmentById(R.id.map) as SupportMapFragment
+        fragment.getMapAsync(this)
 
-        // Example of a call to a native method
-        sample_text.text = stringFromJNI()
+
     }
 
+    override fun onMapReady(googleMap: GoogleMap) {
+        val sydney = LatLng(-34.0, 151.0)
+        googleMaps = googleMap
+        googleMaps.addMarker(MarkerOptions().position(sydney).title("Marker in Sydeny!"))
+        googleMaps.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
 }
